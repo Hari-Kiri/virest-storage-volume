a66a4ab0a0fd6cd3ea212b9db793dabb8195bd7e
Hari-Kiri <pratamahari89@gmail.com>
2025-01-11 21:21:07 +0000

    feat(volume-create): add option

--- structures/volumeCreate/Request.go
+++ structures/volumeCreate/Request.go
@@ -4,5 +4,6 @@ import "libvirt.org/go/libvirtxml"
 
 type Request struct {
 	PoolUuid      string                   `json:"poolUuid"`
+	Option        uint                     `json:"option"`
 	StorageVolume libvirtxml.StorageVolume `json:"storageVolume"`
 }
